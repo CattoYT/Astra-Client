42f328f5fe1df1d36d0b6cbe8398fa2322671478
CattoYT <97292582+CattoYT@users.noreply.github.com>
2022-03-19 21:18:10 +0000

    updated to have movement modules

--- me/catto/astra/module/ModuleManager.java
+++ me/catto/astra/module/ModuleManager.java
@@ -5,8 +5,6 @@ import me.catto.astra.module.movement.*;
 
 import java.util.ArrayList;
 
-import me.catto.astra.module.movement.Sprint;
-
 public class ModuleManager {
 
     private static ArrayList<Module> mods;
@@ -14,9 +12,15 @@ public class ModuleManager {
     public ModuleManager() {
         mods = new ArrayList<Module>();
         //COMBAT
+        newMod(new FastBow());
         newMod(new Aura());
         //MOVEMENT
         newMod(new Sprint());
+        newMod(new AutoWalk());
+        newMod(new Dolphin());
+        newMod(new Fly());
+        newMod(new NoFall());
+        newMod(new Parkour());
         //PLAYER
 
         //RENDER
